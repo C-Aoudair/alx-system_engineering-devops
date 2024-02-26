9bb2db7090611fde3093bfee14d51ddc15ba22a4
AOUDAI <aoudair16031999@gmail.com>
2024-02-26 11:52:40 +0100

    fix task 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
 # install flask from pip3
 
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3'
+exec { 'install_flask_2.1.0':
+  command => '/usr/bin/pip3 install flask==2.1.0',
+  unless  => '/usr/bin/pip3 show flask | grep -q "Version: 2.1.0"',
 }
