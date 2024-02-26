d7e882fbf0358e7d4d870f435b7c1d00b50bba21
AOUDAI <aoudair16031999@gmail.com>
2024-02-26 12:20:38 +0100

    test

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,10 +1,11 @@
 # install flask from pip3
-package { 'pip3':
-  ensure => 'installed'
+
+package { 'python3-pip':
+  ensure => 'installed',
 }
 
 package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
-  require  => package['pip3']
+  require  => package['python3-pip'],
 }
