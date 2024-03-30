207b8b1409197d8912c703f1d5c3068c053e8e8e
AOUDAI <aoudair16031999@gmail.com>
2024-03-30 16:08:07 +0100

    test

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -6,25 +6,42 @@ import json
 import requests
 
 
-if __name__ == "__main__":
+def main():
+    """ the main function"""
 
+    users = requests.get(f'https://jsonplaceholder.typicode.com/users')
     r = requests.get('https://jsonplaceholder.typicode.com/todos')
-
+    todos = r.json()
     data = {}
-    for Id in range(1, 11):
-        request = requests.get(
-                f'https://jsonplaceholder.typicode.com/users/{Id}'
-                )
-        name = request.json().get('username')
 
-        data[Id] = []
-        for task in r.json():
-            if task.get('userId') == Id:
-                title = task.get('title')
-                status = task.get('completed')
-                data[Id].append(
-                    {'username': name, 'task': title, 'completed': status}
-                    )
+    for Id in range(1, 11):
+        userName = get_username(users.json()[Id - 1])
+        data[Id] = get_data(todos, userName, Id)
 
     with open(f"todo_all_employees.json", 'w') as file:
         json.dump(data, file)
+
+
+def get_username(data):
+    """ return username from dictionary data"""
+    return data.get('username')
+
+
+def get_data(data, name, Id):
+    """ return information about a given user from API data"""
+
+    userList = []
+
+    for task in data:
+        if task.get('userId') == Id:
+            title = task.get('title')
+            status = task.get('completed')
+            userList.append(
+                    {'username': name, 'task': title, 'completed': status}
+                    )
+
+    return userList
+
+
+if __name__ == "__main__":
+    main()
