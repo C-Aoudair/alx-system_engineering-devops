ea56ecb76abb099b985f6d5f205656711c74b0fe
AOUDAI <aoudair16031999@gmail.com>
2024-04-10 14:55:06 +0000

    test

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -8,15 +8,15 @@ def top_ten(subreddit):
     """ Document"""
 
     request = requests.get(
-        "https://www.reddit.com/r/{}/hot.json".format(subreddit),
-        headers={"User-Agent": "Custom"},
+        f"https://www.reddit.com/r/{subreddit}/hot.json",
+        headers={"User-Agent": "MyPythonScript/1.0 (Ubuntu 20.04; Python 3.4.3)"},
         params={"limit": 10},
     )
 
     if request.status_code == 200:
-        for get_data in request.json().get("data").get("children"):
-            dat = get_data.get("data")
-            title = dat.get("title")
+        for resulte in request.json().get("data").get("children"):
+            post = resulte.get("data")
+            title = post.get("title")
             print(title)
     else:
         print(None)
