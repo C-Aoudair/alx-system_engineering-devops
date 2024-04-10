a4a61d4091d21d47cb72c4d05c49db49261f00f4
AOUDAI <aoudair16031999@gmail.com>
2024-04-10 14:11:16 +0000

    test

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,8 +8,11 @@ def number_of_subscribers(subreddit):
     Retures the number of subscribers for a given subreddit
     - If not a valid subreddit, return 0.
     """
-    r = requests.get(f'https://www.reddit.com/r/{subreddit}/about.json',
-                     headers={"User-Agent": "Custom"},
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    r = requests.get("https://www.reddit.com/r/{}/about.json".format(subreddit),
+                     headers=headers,
                      allow_redirects=False)
 
     if r.status_code == 404:
