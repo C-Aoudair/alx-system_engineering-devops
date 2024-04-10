f11d43760fe1fdf1ce9d441cf98bc05b1f972040
AOUDAI <aoudair16031999@gmail.com>
2024-04-10 14:00:09 +0000

    test

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,15 +1,12 @@
 #!/usr/bin/python3
 """ Contains number_of_subscribers function"""
-
+import requests
 
 def number_of_subscribers(subreddit):
     """
     Retures the number of subscribers for a given subreddit
     - If not a valid subreddit, return 0.
     """
-
-    import requests
-
     r = requests.get(f'https://www.reddit.com/r/{subreddit}/about.json',
                      headers={"User-Agent": "Custom"},
                      allow_redirects=False)
